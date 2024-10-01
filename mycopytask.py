90e06870091f72fd688b0978bf4ea085e3d0985b
Jobert Agdahan <jobertagdahan47@gmail.com>
2024-10-01 11:44:34 +0800

    Add files via upload

---
+++ mycopytask.py
@@ -0,0 +1,15 @@
+favfruitsnum = int(input("Enter number of fruit: "))
+numberoffruits = []
+
+for i in range(favfruitsnum):
+   name = input("Enter a name of fruits: ")
+   numberoffruits.append(name)
+
+print(numberoffruits)
+   
+for fuits in fruits:
+   if fruit == "Mango":
+      print("Mango")
+      break
+   elif fruit == "Banana":
+      print("Happy eating")
